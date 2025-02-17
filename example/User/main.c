fd86d707dbed0e01ecc5adca4f017a8ce2863ba3
xidianswq <3209507800@qq.com>
2025-02-17 09:25:12 +0800

    upload

---
+++ example/User/main.c
@@ -0,0 +1,183 @@
+//*********************************************头文件声明*********************************************
+#include "public.h"
+#include "bluetooth.h"
+#include "oled.h"
+#include "Pwm_1.h"
+#include "Pwm_2.h"
+#include "dc_motor.h"
+#include "servo_motor.h"
+#include "AD.h"
+#include "string.h"	
+#include "Delay.h"
+#include "Timer.h"
+#include "cs100a.h"
+#include "usart.h"
+#include "MPU6050.h"
+#include "beep.h"
+#include "led.h"
+#include "ray.h"
+#include "pca9685.h"
+#include "dma.h"
+
+//********************************************引脚使用说明********************************************
+/*
+oled.h				GPIOA PIN0/1
+bluetooth.h			GPIOA PIN2/3
+AD.h				GPIOA PIN4/5 ADC1_CH4/5
+Pwm_2.h				GPIOA PIN6/7 TIM3_CH1/2 20hz
+Pwm_1.h				GPIOA PIN8 TIM1_CH1 50hz
+usart.h				GPIOA PIN9/10 TX/RX Black/White
+ray.h				GPIOA PIN11/12 GPIOB PIN0/1/5
+dc_motor.h			GPIOB PIN3/4/6/7 TIM4_CH1/2
+pca9685.h			GPIOB PIN8/9 I2C_1(SCL/SDA) 
+mpu6050.h			GPIOB PIN10/11 I2C_2
+cs100a.h			GPIOB PIN12/13 
+beep.h				GPIOB PIN14
+led.h				GPIOB PIN15
+Timer.h				TIM2 10hz
+*/
+
+//********************************************全局变量声明********************************************
+extern float Voltage[2];	//ad测量电压值[0.3.3]			//ad.c
+extern int Duty;			//直流电机pwm波占空比[0,500]		//dc_motor.c
+extern float distance;		//超声波测距值					//cs100a.c
+extern float Speed_Cm_S;	//小车速度cm/s					//dc_motor.c
+extern char  USART_RX_INFO[USART_REC_LEN];	//uart接收数据	//usart.c
+extern int x,y;												//PCA9685.c
+
+int16_t Ax,Ay,Az,Pitch,Roll,Yaw;			//mpu6050接收到的数据
+int mode=0;					//当前功能模式
+
+void system_init(void){
+	//********************初始化程序********************
+	BlueToothInit(9600,USART_Parity_No,USART_StopBits_1,USART_WordLength_8b);	//蓝牙初始化
+	OLED_Init();				//oled初始化
+	Beep_Init();				//蜂鸣器初始化
+	Led_Init();					//led灯初始化
+	//Ray_Init();					//红外模块初始化
+	//MPU6050_Init();				//mpu6050初始化
+	uart_init(115200);			//uart1初始化
+	Encoder_Init();				//直流电机编码器初始化
+	AD_Init();					//ad初始化
+	Timer_Init();				//0.1s定时器初始化
+	Cs100a_Init();				//超声波测距初始化
+	PCA9685_Init();				//PCA9685初始化
+	PWM_Init();					//直流电机pwm初始化
+	TIM1_PWM_Init(9999,143);	//一周期20ms，分辨率20ms/10000）
+	TIM_SetCompare1(TIM1,750);	//对齐角度为90度(1.5ms)
+	Dc_Motor_Init();			//直流电机初始化（必须是最后配置的GPIO！）
+}
+
+//********************************************测试程序部分********************************************
+void mpu6050_test(void){
+	//mpu6050测试
+	while(1)
+	{
+		MPU6050_GetData(&Ax,&Ay,&Az,&Pitch,&Roll,&Yaw);
+		if(Ax==0||Ax==-11823)MPU6050_Init();			//mpu6050出错重启
+		BlueTooth_TransmitPrintf("Ax:%d\r\nAy:%d\r\nAz:%d\r\npitch:%d\r\nrow:%d\r\nyaw:%d\r\n",Ax,Ay,Az,Pitch,Roll,Yaw);
+	}
+}
+
+void function_test(void){
+	//功能测试
+	while(1)
+	{
+		
+		//蓝牙接收信号处理部分
+		if(Get_BlueTooth_ReceivePackFlag()==1)
+		{
+			BlueTooth_TransmitPrintf("%s\r\n",BlueTooth_RxPack);
+			BlueTooth_TransmitPrintf("\r\nOK\r\n");
+			mode=BlueTooth_RxPack[0];
+		}
+		switch(mode){
+			case 0:Dc_Motor_Stop();break;
+			case 1:Follow_Track(50);break;
+			case 2:Follow_Point();break;
+			case 3:Dc_Motor_Mode();break;
+			case 4:JoyStick_Control();break;
+			case 5:Simple_Aviod_Obstacle();break;
+			case 6:break;
+			case 7:break;
+			case 8:break;
+			case 9:break;	
+			case 10:break;
+			case 11:break;
+			case 12:break;
+			case 13:break;
+			case 14:break;
+			case 15:break;
+		}
+	}
+}
+
+//*********************************************主函数部分*********************************************
+int main(void)
+{	
+	system_init();
+	function_test();
+}
+
+//*********************************************中断函数部分*********************************************
+//0.1s一次中断(oled状态刷新)	
+void TIM2_IRQHandler()
+{
+	if (TIM_GetITStatus(TIM2, TIM_IT_Update) == SET)
+	{	
+		//基本状态显示
+		Cs100a_Start();
+		Show_Voltage_State();
+		Show_DC_Motor_State();	
+		Show_Distance();
+		
+		//显示红外传感器状态
+		OLED_ShowNum(2,11,L_2,1);
+		OLED_ShowNum(2,12,L_1,1);
+		OLED_ShowNum(2,13,C,1);
+		OLED_ShowNum(2,14,R_1,1);
+		OLED_ShowNum(2,15,R_2,1);
+		
+		//uart接收处理显示
+		//Get_Point_Pos();
+		OLED_ShowNum(3,12,x,3);
+		//OLED_ShowString(3,12,USART_RX_INFO);
+		
+		TIM_ClearITPendingBit(TIM2, TIM_IT_Update);
+		
+		//显示当前角度
+//		static int times=0;
+//		times++;
+//		if(times==50)
+//		{
+//			times=0;
+//			MPU6050_GetData(&Ax,&Ay,&Az,&Pitch,&Roll,&Yaw);
+//			if(Ax==0||Ax==-11823)MPU6050_Init();			//mpu6050出错重启
+//			BlueTooth_TransmitPrintf("Ax:%d\r\nAy:%d\r\nAz:%d\r\npitch:%d\r\nrow:%d\r\nyaw:%d\r\n",Ax,Ay,Az,Pitch,Roll,Yaw);
+//		}
+
+
+	}
+}
+
+//超声波模块中断函数
+void EXTI15_10_IRQHandler()
+{
+	static int temp;
+	int counter;
+	//在cs100a的echo引脚发送脉冲信号的上升沿和下降沿触发
+	if (EXTI_GetITStatus(EXTI_Line13) == SET)
+	{
+		counter=TIM_GetCounter(TIM2);
+		
+		//确保下降沿计数值比上升沿大，相减即可实现间隔时间的测量，从而不用使用while循环等待下降沿到来
+		if(counter>temp)
+		{
+			time=counter-temp;
+			temp=counter;
+		}
+		else temp=counter;
+		
+		EXTI_ClearITPendingBit(EXTI_Line13);
+	}
+}
